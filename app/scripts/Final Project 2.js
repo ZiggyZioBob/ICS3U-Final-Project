7c61712c642b5a4a3164ea128a7a878c992cae2e
1801-AD-9FW16M2\outreach3 <liuliuliucnca@gmail.com>
2018-08-01 15:54:56 -0400

    Extensive Stopwatch modifications

--- app/scripts/Final Project 2.js
+++ app/scripts/Final Project 2.js
@@ -7,7 +7,14 @@ var tenminutes = 0;
 var hours = 0;
 var Stopcounter = 0;
 var Runcounter = 0;
+var ArrayLapDisplay = [];
+var Lapcounter = 0;
+var LapnumberCounter = 0;
+var Lapdisplayvalue = [];
+var ThisCounter = 0;
 var Counternow = "0:00:00:00";
+var y = 22;
+var x = 10;
 
 function Start() {
 	Runcounter = 1;
@@ -64,14 +71,57 @@ function Reset() {
 		tenminutes = 0;
 		hours = 0;
 		Resetcounter = 0;
+		Lapcounter = 0;
+		LapnumberCounter = 0;
 		Counternow = "0:00:00:00";
 		document.getElementById("Haha").innerHTML = Counternow;
+		ArrayLapDisplay = [];
+		document.getElementById("Lapdisplay").innerHTML = ArrayLapDisplay;
 	}
 }
 
 function Lap() {
 	if(Runcounter == 1){
-		debugger;
-		document.getElementById("Lapdisplay").innerHTML = document.getElementById("Haha").innerHTML + "<br/>";
+		LapnumberCounter = LapnumberCounter + 1;
+		ArrayLapDisplay = ArrayLapDisplay + "Lap " + LapnumberCounter + ": " + document.getElementById("Haha").innerHTML;
+		ArrayLapDisplay = ArrayLapDisplay + "</br>";
+		Lapcounter = 1;
+		document.getElementById("Lapdisplay").innerHTML = ArrayLapDisplay;
 	}
+}
+
+function FindLap() {
+	debugger;
+	if(Lapcounter == 1){
+		var Lapfindvalue1 = parseInt(document.getElementById("Lapbox1").value);
+		var Lapfindvalue2 = parseInt(document.getElementById("Lapbox2").value);
+		ThisCounter = Lapfindvalue1;
+		while(ThisCounter >= x){
+			y = y + 1;
+			x = x * 10;
+		}
+		Lapfindvalue1 = (Lapfindvalue1 - 1) * y - 9;
+		do {
+			while(ThisCounter >= x){
+				y = y + 1;
+				x = x * 10;
+			}
+			for(var m = 0; m < y; m++){
+				Lapdisplayvalue = Lapdisplayvalue + ArrayLapDisplay[Lapfindvalue1 + m];
+			}
+			Lapfindvalue1 = Lapfindvalue1 + y;
+			ThisCounter = ThisCounter + 1;
+		} while (ThisCounter <= Lapfindvalue2);
+		document.getElementById("Lapdisplay").innerHTML = Lapdisplayvalue;
+		ThisCounter = 0;
+		Lapdisplayvalue = [];
+		y = 22;
+		x = 10;
+		document.getElementById("Lapbox1").value = "";
+		document.getElementById("Lapbox2").value = "";
+	}
+}
+
+function Return() {
+	document.getElementById("Lapdisplay").innerHTML = ArrayLapDisplay;
 }
